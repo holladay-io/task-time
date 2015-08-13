d9c5f55d4f86c5824563682637e0261dfb160f70
Joshua Holladay <jholladay@Erics-iPhone-8.arc.nasa.gov>
2015-08-12 17:38:19 -0700

    Created simple view with label and button. Enabled button toggle to start and stop timer.

--- PomTime/AppDelegate.swift
+++ PomTime/AppDelegate.swift
@@ -12,7 +12,12 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var timeLabel: NSTextField!
+    @IBOutlet weak var startStopButton: NSButton!
+    
+    let workTime: NSTimeInterval = 10.0
+    let restTime: NSTimeInterval = 5.0
+    var isTiming = false
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -22,6 +27,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func toggleTimer(sender: AnyObject) {
+        if isTiming {
+            isTiming = false
+            startStopButton.title = "Start"
+        } else {
+            isTiming = true
+            startStopButton.title = "Stop"
+        }
+    }
 
 }
 
